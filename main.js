13b425439acba55586ee6d6a8d8c7fafb398be75
Allan <al.silvaquiroz@gmail.com>
2022-12-26 16:25:16 -0300

    feat: minor changes in app.js, app.html  and main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import './style.css'
+import './style.css';
+import './normalize.css';
 import { App } from './src/app';
 
 App('#app');
--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import html from './app.html?raw';
-
+import { renderLoader } from './use-cases/loader';
 
 export const App = (elementId) => {
 
     (() => {
         const app = document.querySelector(elementId);
         app.innerHTML = html;
+        renderLoader();
+
     })();
 
 }
