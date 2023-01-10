0b0497d33272232fbfa28fbf705d26600802f268
Allan <al.silvaquiroz@gmail.com>
2023-01-10 17:21:24 -0300

    feat: build changes required

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import html from './app.html?raw';
 import { atom } from './use-cases/atom';
 import { smooth } from './use-cases/lenis';
 // import { renderLoader } from './use-cases/loader';
-import { parallaxMouse, parallaxScroll } from './use-cases/parallax';
 import { reveal } from './use-cases/reveal';
 import { move } from './use-cases/move';
 import { formUtils } from './use-cases/form';
@@ -12,6 +11,8 @@ import { showDemo } from './use-cases/scrollText';
 import { media } from './use-cases/media';
 import { onclick } from './use-cases/onclick';
 import { knowledge } from './use-cases/knowledge';
+import { color } from './use-cases/color';
+import { lateral } from './use-cases/lateral';
 
 
 export const App = (elementId) => {
@@ -20,17 +21,16 @@ export const App = (elementId) => {
         const app = document.querySelector(elementId);
         app.innerHTML = html;
         // renderLoader();
-        // parallaxMouse();
-
-        parallaxScroll();
         atom();
         reveal();
         knowledge();
+        lateral();
         if (!(window.matchMedia("( max-width: 900px )").matches)) {
             smooth();
             showDemo();
             move();
             onclick();
+            color();
         }
         onKeyup();
         media();
